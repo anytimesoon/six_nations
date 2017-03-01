5d31a36a65b0e4ccc285ea8481dcb8217c60cc4c
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-01 22:51:39 +0000

    added dummy code to CLI.rb to start basic idea for interface

--- lib/six_nations/cli.rb
+++ lib/six_nations/cli.rb
@@ -3,8 +3,33 @@ class SixNations::CLI
   def start
     puts <<~DOC
       Welcome to the Six Nations Ruby Gem
-      Would you like to see the current table, upcomming fixtures, or find out more about a team?
+      Would you like to see:
+      1. current table
+      2. upcomming fixtures
+      3. find out more about a team
+      4. exit
     DOC
-    input = gets.strip.downcase
+
+    input = ""
+    
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1" || "current table"
+        puts "current table"
+      when "2" || "upcomming fixtures"
+        puts "upcomming fixtures"
+      when "3" || "find out more about a team"
+        puts "Which team would you like to know about?"
+        requested_team = gets.strip.downcase
+        puts "What would you like to know about #{requested_team}?"
+        team_info = gets.strip.downcase
+        puts "Information you have requested"
+      when "exit"
+        puts "Thanks for playing. See you next time"
+      else
+        puts "I'm not sure what you mean, please choose 1, 2, 3 or exit."
+      end
+    end
   end
 end
