4cb1e1eb330c58f8636799b38b110e8dbe492235
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-02 18:16:13 +0000

    added @@all array and #create_all

--- lib/six_nations/team.rb
+++ lib/six_nations/team.rb
@@ -1,3 +1,15 @@
 class SixNations::team
-  
+  attr_accessor :stats, :players, :fixtures
+  attr_reader :nation
+
+  @@all = []
+
+  def initialize(nation)
+    @nation = nation
+    self.all << self
+  end
+
+  def create_all
+    [France, England, Wales, Scotland, Ireland, Italy].each {|country| self.new(country) }
+  end
 end
