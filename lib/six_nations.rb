1a0abd141a439e5b4617f3108a676a2c5f667623
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-02 20:18:45 +0000

    Added .get_stats with dummy values for variables

--- lib/six_nations.rb
+++ lib/six_nations.rb
@@ -1,3 +1,6 @@
 require_relative "six_nations/version"
 require_relative "six_nations/cli"
 require_relative "six_nations/team"
+
+
+require "pry"
--- lib/six_nations/team.rb
+++ lib/six_nations/team.rb
@@ -1,12 +1,13 @@
 module SixNations
   class Team
     attr_accessor :stats, :players, :fixtures
-    attr_reader :nation
+    attr_reader :name
 
     @@all = []
 
-    def initialize(nation)
-      @nation = nation
+    def initialize(name)
+      @name = name
+      @stats = {}
       @@all << self
     end
 
@@ -14,8 +15,26 @@ module SixNations
       @@all
     end
 
-    def create_all
-      [France, England, Wales, Scotland, Ireland, Italy].each {|country| self.new(country) }
+    def self.create_all
+      ["France", "England", "Wales", "Scotland", "Ireland", "Italy"].each {|nation| self.new(nation) }
     end
-  end
-end
+
+    def self.get_stats
+      @@all.each do |nation|
+        binding.pry
+        nation.stats[:position] = "1"
+        nation.stats[:games_played] = "3"
+        nation.stats[:win] = "2"
+        nation.stats[:draw] = "0"
+        nation.stats[:loss] = "1"
+        nation.stats[:points_for] = "5"
+        nation.stats[:points_agains] = "6"
+        nation.stats[:point_diff] = "4"
+        nation.stats[:try_bonus] = "6"
+        nation.stats[:user_bonus] = "5"
+        nation.stats[:points] = "4"
+      end
+    end
+
+  end#of Team
+end#of SixNations
