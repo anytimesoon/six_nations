caf62657c42655f9d168e19f094bf9efd4756b96
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-02 21:40:23 +0000

    Added #display_table

--- lib/six_nations/team.rb
+++ lib/six_nations/team.rb
@@ -1,49 +1,47 @@
-module SixNations
-  class Team
-    attr_accessor :stats, :players, :fixtures
-    attr_reader :name
+class SixNations::Team
+  attr_accessor :stats, :players, :fixtures
+  attr_reader :name
 
-    @@all = []
+  @@all = []
 
-    def initialize(name)
-      @name = name
-      @stats = {}
-      @players = []
-      @fixtures = []
-      @@all << self
+  def initialize(name)
+    @name = name
+    @stats = {}
+    @players = []
+    @fixtures = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.create_all(nations)
+    (nations).map {|nation| self.new(nation) }
+  end
+
+  def self.get_stats
+    @@all.each do |nation|
+      nation.stats[:position] = "1"
+      nation.stats[:games_played] = "3"
+      nation.stats[:win] = "2"
+      nation.stats[:draw] = "0"
+      nation.stats[:loss] = "1"
+      nation.stats[:points_for] = "5"
+      nation.stats[:points_agains] = "6"
+      nation.stats[:point_diff] = "4"
+      nation.stats[:try_bonus] = "6"
+      nation.stats[:user_bonus] = "5"
+      nation.stats[:points] = "4"
     end
+  end
 
-    def self.all
-      @@all
-    end
+  def get_players
+    @players << SixNations::Player.create_all
+  end
 
-    def self.create_all(nations)
-      (nations).map {|nation| self.new(nation) }
-    end
+  def get_fixtures
+    @fixtures << SixNations::Fixtures.create_all
+  end
 
-    def self.get_stats
-      @@all.each do |nation|
-        nation.stats[:position] = "1"
-        nation.stats[:games_played] = "3"
-        nation.stats[:win] = "2"
-        nation.stats[:draw] = "0"
-        nation.stats[:loss] = "1"
-        nation.stats[:points_for] = "5"
-        nation.stats[:points_agains] = "6"
-        nation.stats[:point_diff] = "4"
-        nation.stats[:try_bonus] = "6"
-        nation.stats[:user_bonus] = "5"
-        nation.stats[:points] = "4"
-      end
-    end
-
-    def get_players
-      @players << SixNations::Player.create_all
-    end
-
-    def get_fixtures
-      @fixtures << SixNations::Fixtures.create_all
-    end
-
-  end#of Team
-end#of SixNations
+end#of Team
--- lib/six_nations/tournament.rb
+++ lib/six_nations/tournament.rb
@@ -1,15 +1,21 @@
 class SixNations::Tournament
   attr_accessor :teams, :nations
-
-  
   
   def initialize
-    @teams = []
     @nations = ["France", "England", "Wales", "Scotland", "Ireland", "Italy"]
+    @teams = SixNations::Team.create_all(@nations)
     SixNations::Team.get_stats
   end
 
-  def get_teams
-    @teams = SixNations::Team.create_all(@nations)
+  def display_table
+    current_ranking = team_ranker
+    
+    puts "Pos | Team  | Pl | W | D | L | PF | PA | Diff | TF | TA | TB | LB | Pts"
+    puts current_ranking[0]
+    puts current_ranking[1]
+    puts current_ranking[2]
+    puts current_ranking[3]
+    puts current_ranking[4]
+    puts current_ranking[5]
   end
-end
+end#of Tournament
