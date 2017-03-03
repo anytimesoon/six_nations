80cade8349df37a8986289ed6c4bcb772aab410e
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-03 22:52:50 +0000

    team class scrapes stats successfully for each team

--- lib/six_nations/team.rb
+++ lib/six_nations/team.rb
@@ -3,13 +3,13 @@ class SixNations::Team
   attr_reader :name, :url
 
   @@all = []
+  @@url = "http://www.rbs6nations.com/en/matchcentre/league_table.php"
 
   def initialize(name)
     @name = name
     @stats = {}
     @players = []
     @fixtures = []
-    @url = "http://www.rbs6nations.com/en/matchcentre/league_table.php"
     @@all << self
   end
 
@@ -21,24 +21,29 @@ class SixNations::Team
     (nations).map {|nation| self.new(nation) }
   end
 
-  def get_stats
-    html = open("@url")
+  def self.get_stats
+    html = open(@@url)
     doc = Nokogiri::HTML(html)
+    table = doc.css('.leaguetab')
     
     @@all.each do |nation|
-      nation.stats[:position] = doc.css(".field_Position").text
-      nation.stats[:games_played] = doc.css(".field_Played").text
-      nation.stats[:win] = doc.css(".field_Win").text
-      nation.stats[:draw] = doc.css(".field_Draw").text
-      nation.stats[:loss] = doc.css(".field_Lose").text
-      nation.stats[:points_for] = doc.css(".field_PtsFor").text
-      nation.stats[:points_against] = doc.css(".field_PtsAgainst").text
-      nation.stats[:point_diff] = doc.css(".field_PtsDiff").text
-      nation.stats[:try_for] = doc.css(".field_TryFor").text
-      nation.stats[:try_against] = doc.css(".field_TryAgainst").text
-      nation.stats[:try_bonus] = doc.css(".field_TryBonus").text
-      nation.stats[:loser_bonus] = doc.css(".field_LoseBonus").text
-      nation.stats[:points] = doc.css(".field_Points").text
+
+      row = table.search "[text()*='#{nation.name.strip}']"
+      row = row.first.parent
+      
+      nation.stats[:position] = row.css(".field_Position").text
+      nation.stats[:games_played] = row.css(".field_Played").text
+      nation.stats[:win] = row.css(".field_Win").text
+      nation.stats[:draw] = row.css(".field_Draw").text
+      nation.stats[:loss] = row.css(".field_Lose").text
+      nation.stats[:points_for] = row.css(".field_PtsFor").text
+      nation.stats[:points_against] = row.css(".field_PtsAgainst").text
+      nation.stats[:point_diff] = row.css(".field_PtsDiff").text
+      nation.stats[:try_for] = row.css(".field_TryFor").text
+      nation.stats[:try_against] = row.css(".field_TryAgainst").text
+      nation.stats[:try_bonus] = row.css(".field_TryBonus").text
+      nation.stats[:loser_bonus] = row.css(".field_LoseBonus").text
+      nation.stats[:points] = row.css(".field_Points").text
     end
   end
 
