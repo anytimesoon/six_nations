66808fe63d3b34fa028f07f74532f72462ca4754
Damien de Lartigue <damien@delartigue.co.uk>
2017-03-03 22:32:20 +0000

    scraping for team stats

--- lib/six_nations/team.rb
+++ lib/six_nations/team.rb
@@ -9,7 +9,7 @@ class SixNations::Team
     @stats = {}
     @players = []
     @fixtures = []
-    @url = "http://www.rbs6nations.com/en/#{@name.downcase.strip}"
+    @url = "http://www.rbs6nations.com/en/matchcentre/league_table.php"
     @@all << self
   end
 
@@ -21,21 +21,24 @@ class SixNations::Team
     (nations).map {|nation| self.new(nation) }
   end
 
-  def self.get_stats
+  def get_stats
+    html = open("@url")
+    doc = Nokogiri::HTML(html)
+    
     @@all.each do |nation|
-      nation.stats[:position] = "1" #to be scraped
-      nation.stats[:games_played] = "3" #to be scraped
-      nation.stats[:win] = "2" #to be scraped
-      nation.stats[:draw] = "0" #to be scraped
-      nation.stats[:loss] = "1" #to be scraped
-      nation.stats[:points_for] = "5" #to be scraped
-      nation.stats[:points_against] = "6" #to be scraped
-      nation.stats[:point_diff] = "4" #to be scraped
-      nation.stats[:try_for] = "7" #to be scraped
-      nation.stats[:try_against] = "3" #to be scraped
-      nation.stats[:try_bonus] = "6" #to be scraped
-      nation.stats[:loser_bonus] = "5" #to be scraped
-      nation.stats[:points] = "4" #to be scraped
+      nation.stats[:position] = doc.css(".field_Position").text
+      nation.stats[:games_played] = doc.css(".field_Played").text
+      nation.stats[:win] = doc.css(".field_Win").text
+      nation.stats[:draw] = doc.css(".field_Draw").text
+      nation.stats[:loss] = doc.css(".field_Lose").text
+      nation.stats[:points_for] = doc.css(".field_PtsFor").text
+      nation.stats[:points_against] = doc.css(".field_PtsAgainst").text
+      nation.stats[:point_diff] = doc.css(".field_PtsDiff").text
+      nation.stats[:try_for] = doc.css(".field_TryFor").text
+      nation.stats[:try_against] = doc.css(".field_TryAgainst").text
+      nation.stats[:try_bonus] = doc.css(".field_TryBonus").text
+      nation.stats[:loser_bonus] = doc.css(".field_LoseBonus").text
+      nation.stats[:points] = doc.css(".field_Points").text
     end
   end
 
